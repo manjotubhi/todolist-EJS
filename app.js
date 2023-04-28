113c62dc131eb734a797356f6710a6e7ed97d2fc
manjot ubhi <manjotubhi@yahoo.com>
2023-04-28 14:32:36 -0400

    practice ejs in branch

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+// let ejs = require('ejs');
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
 
+// app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-    const today = new Date()
-    if (today.getDate() === 6) {
-        res.send("yay it's weekend")
-    } else {
-        res.send("Boo! I have to work today")
+    const today = new Date();
+    const currentDay = today.getDay();
+    const day = "";
+
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+
+        default:
+
+            break;
     }
+
+    res.render('list', {
+        kindOfDay: day
+    });
 })
 
 app.listen(port, () => {
