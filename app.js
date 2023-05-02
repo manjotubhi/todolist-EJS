8499930f95931f093946d27eeb68be588a1a83ff
manjot ubhi <manjotubhi@yahoo.com>
2023-05-02 12:18:55 -0400

    add date with localeDateString

--- app.js
+++ app.js
@@ -11,36 +11,42 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     const today = new Date();
     const currentDay = today.getDay();
-    let day = "";
 
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-
-        default:
-
-            break;
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+    let day = today.toLocaleDateString("en-US", options)
+
+    // switch (currentDay) {
+    //     case 0:
+    //         day = "Sunday"
+    //         break;
+    //     case 1:
+    //         day = "Monday"
+    //         break;
+    //     case 2:
+    //         day = "Tuesday"
+    //         break;
+    //     case 3:
+    //         day = "Wednesday"
+    //         break;
+    //     case 4:
+    //         day = "Thursday"
+    //         break;
+    //     case 5:
+    //         day = "Friday"
+    //         break;
+    //     case 6:
+    //         day = "Saturday"
+    //         break;
+
+    //     default:
+
+    //         break;
+    // }
 
     res.render('list', {
         kindOfDay: day
