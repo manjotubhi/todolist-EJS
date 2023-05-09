a00718a3d0bdbd59900d852d956e96d79564f67f
manjot ubhi <manjotubhi@yahoo.com>
2023-05-09 12:25:04 -0400

    modified app.js and list.ejs

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 // let ejs = require('ejs');
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
 
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     const today = new Date();
-    const currentDay = today.getDay();
-
 
     let options = {
         weekday: "long",
@@ -20,38 +19,20 @@ app.get('/', (req, res) => {
     }
     let day = today.toLocaleDateString("en-US", options)
 
-    // switch (currentDay) {
-    //     case 0:
-    //         day = "Sunday"
-    //         break;
-    //     case 1:
-    //         day = "Monday"
-    //         break;
-    //     case 2:
-    //         day = "Tuesday"
-    //         break;
-    //     case 3:
-    //         day = "Wednesday"
-    //         break;
-    //     case 4:
-    //         day = "Thursday"
-    //         break;
-    //     case 5:
-    //         day = "Friday"
-    //         break;
-    //     case 6:
-    //         day = "Saturday"
-    //         break;
-
-    //     default:
-
-    //         break;
-    // }
-
     res.render('list', {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     });
 })
+
+app.post('/', (req, res) => {
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
